e84893be3f190174fb2adecf6de3e02225f9d95a
sofiya rukadikar <116500687+sofiyarukadikar@users.noreply.github.com>
2026-03-28 23:19:33 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ftgso-sim",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=["numpy", "matplotlib", "pandas", "seaborn"],
-    python_requires=">=3.8",
-)
